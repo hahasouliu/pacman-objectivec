7e84ae3dfe5cfbad97b9c6cf8c180985e35ed47e
hahasouliu <hahasouliu@timdemacbook-pro.local>
2015-10-14 21:47:27 +0800

    Initial Commit

---
+++ pacman-objectivec/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pacman-objectivec
+//
+//  Created by hahasouliu on 10/14/15.
+//  Copyright © 2015 hahasouliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pacman-objectivec/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pacman-objectivec
+//
+//  Created by hahasouliu on 10/14/15.
+//  Copyright © 2015 hahasouliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
